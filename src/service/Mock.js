9bea1c446c08ef258629293373406ea1c7c407ba
Natsumi <ntahtosmuamsi@icloud.com>
2023-10-18 22:24:39 +0900

    make build work locally

--- src/service/Mock.js
+++ src/service/Mock.js
@@ -4,67 +4,67 @@ export const EXAMPLE_LOGIN = {
 
 export const EXAMPLE_PRODUCTS = [
     {
-        imageUrl: "/images/fejka-artificial-potted-plant-indoor-outdoor-hanging-ivy__0898086_PE782558_S5.JPG",
+        imageUrl: "./images/fejka-artificial-potted-plant-indoor-outdoor-hanging-ivy__0898086_PE782558_S5.JPG",
         name: "Potted Ivy",
         price: "¥ 2000",
         description: "This plant is a very good one. There is a product description that talks about plant care and other details here."
     },
     {
-        imageUrl: "/images/fejka-artificial-potted-plant-indoor-outdoor-monstera__0614197_PE686822_S5.JPG",
+        imageUrl: "./images/fejka-artificial-potted-plant-indoor-outdoor-monstera__0614197_PE686822_S5.JPG",
         name: "Monstera",
         price: "¥ 3200",
         description: "This plant is a very good one. There is a product description that talks about plant care and other details here."
     },
     {
-        imageUrl: "/images/fejka-artificial-potted-plant-indoor-outdoor-palm__0797076_PE766755_S5.JPG",
+        imageUrl: "./images/fejka-artificial-potted-plant-indoor-outdoor-palm__0797076_PE766755_S5.JPG",
         name: "Palm",
         price: "¥ 2700",
         description: "This plant is a very good one. There is a product description that talks about plant care and other details here."
     },
     {
-        imageUrl: "/images/fejka-artificial-potted-plant-with-pot-indoor-outdoor-succulent__0614211_PE686835_S5.JPG",
+        imageUrl: "./images/fejka-artificial-potted-plant-with-pot-indoor-outdoor-succulent__0614211_PE686835_S5.JPG",
         name: "Succulent",
         price: "¥ 900",
         description: "This plant is a very good one. There is a product description that talks about plant care and other details here."
     },
     {
-        imageUrl: "/images/graeddaepple-plant-pot-indoor-outdoor-pink-white__0952322_PE804311_S5.JPG",
+        imageUrl: "./images/graeddaepple-plant-pot-indoor-outdoor-pink-white__0952322_PE804311_S5.JPG",
         name: "Pink Planter",
         price: "¥ 1700",
         description: "This plant is a very good one. There is a product description that talks about plant care and other details here."
     },
     {
-        imageUrl: "/images/kapkrusbaer-hanging-planter-gray-green__0919747_PE789482_S5.JPG",
+        imageUrl: "./images/kapkrusbaer-hanging-planter-gray-green__0919747_PE789482_S5.JPG",
         name: "Hanging Pot",
         price: "¥ 1600",
         description: "This plant is a very good one. There is a product description that talks about plant care and other details here."
     },
     {
-        imageUrl: "/images/fejka-artificial-potted-plant-indoor-outdoor-cactus__0935865_PE792997_S5.JPG",
+        imageUrl: "./images/fejka-artificial-potted-plant-indoor-outdoor-cactus__0935865_PE792997_S5.JPG",
         name: "Cactus",
         price: "¥ 200",
         description: "This plant is a very good one. There is a product description that talks about plant care and other details here."
     },
     {
-        imageUrl: "/images/fejka-artificial-potted-plant-indoor-outdoor-cactus__0935865_PE792997_S5.JPG",
+        imageUrl: "./images/fejka-artificial-potted-plant-indoor-outdoor-cactus__0935865_PE792997_S5.JPG",
         name: "Cactus",
         price: "¥ 200",
         description: "This plant is a very good one. There is a product description that talks about plant care and other details here."
     },
     {
-        imageUrl: "/images/fejka-artificial-potted-plant-indoor-outdoor-cactus__0935865_PE792997_S5.JPG",
+        imageUrl: "./images/fejka-artificial-potted-plant-indoor-outdoor-cactus__0935865_PE792997_S5.JPG",
         name: "Cactus",
         price: "¥ 200",
         description: "This plant is a very good one. There is a product description that talks about plant care and other details here."
     },
     {
-        imageUrl: "/images/fejka-artificial-potted-plant-indoor-outdoor-cactus__0935865_PE792997_S5.JPG",
+        imageUrl: "./images/fejka-artificial-potted-plant-indoor-outdoor-cactus__0935865_PE792997_S5.JPG",
         name: "Cactus",
         price: "¥ 200",
         description: "This plant is a very good one. There is a product description that talks about plant care and other details here."
     },
     {
-        imageUrl: "/images/fejka-artificial-potted-plant-indoor-outdoor-cactus__0935865_PE792997_S5.JPG",
+        imageUrl: "./images/fejka-artificial-potted-plant-indoor-outdoor-cactus__0935865_PE792997_S5.JPG",
         name: "Cactus",
         price: "¥ 200",
         description: "This plant is a very good one. There is a product description that talks about plant care and other details here."
